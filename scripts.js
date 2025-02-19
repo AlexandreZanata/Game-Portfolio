d0eeb367af4c2b2e76258b6508282306fb06b9b4
Alexandre Zanata de Oliveira Vasconcelos <163176324+AlexandreZanata@users.noreply.github.com>
2025-02-19 15:14:48 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,37 @@
-// scripts.js
 document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.nav-list a');
   
     navLinks.forEach(link => {
       link.addEventListener('click', smoothScroll);
     });
+  
+    const searchBar = document.getElementById('search-bar');
+    const searchButton = document.getElementById('search-button');
+  
+    searchButton.addEventListener('click', performSearch);
+  
+    // Adiciona um evento de escuta para o evento 'keyup'
+    searchBar.addEventListener('keyup', function(event) {
+      if (event.key === 'Enter') {
+        performSearch();
+      }
+    });
+  
+    function performSearch() {
+      const query = searchBar.value.toLowerCase();
+      const gameCards = document.querySelectorAll('.game-card');
+  
+      gameCards.forEach(card => {
+        const title = card.querySelector('.game-title').textContent.toLowerCase();
+        const description = card.querySelector('.game-description').textContent.toLowerCase();
+  
+        if (title.includes(query) || description.includes(query)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    }
   
     function smoothScroll(event) {
       event.preventDefault();
@@ -17,3 +44,4 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
   });
+  
